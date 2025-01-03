5b8fe2945e97f09083c319cc2cd7f4b6f0f759f2
Ronaldo da Silva <ronaldo.intermec@gmail.com>
2025-01-03 17:43:55 -0300

    second commit

---
+++ src/Domain/Products/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Category : Entity
+{
+    public bool Active { get; set; } = true;
+
+}
---
+++ src/Domain/Products/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Product : Entity
+{
+    public int CategoryId { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
+    public bool Active { get; set; } = true;
+
+
+}
