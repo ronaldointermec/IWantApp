1cea0c50094bf4fbbf6e1216766fc14849e79d2d
Ronaldo da Silva <ronaldo.intermec@gmail.com>
2025-01-07 21:47:22 -0300

    added Identiy

--- src/obj/Debug/net6.0/IWantApp.AssemblyInfo.cs
+++ src/obj/Debug/net6.0/IWantApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e549ead07553ab1f3ba5ec86280b137eee28ad81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd81eef38ef451b20adadc436d49e823efd0b08")]
 [assembly: System.Reflection.AssemblyProductAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
